eacc0b6315169d832c8e0a51f5d83db0f5417de6
Bladimir Peralta Herrera <bladimir.peralta@iescesarmanrique.com>
2024-05-15 00:04:29 +0100

    nuevas implementaciones

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,22 +2,68 @@
     <nav class="navbar-contenedor">
         <img class="logo" src="../assets/img/logos/logo3.png">
         <div class="items">
-            <button class="item" @click="callChangeContent({ contenido: 'aboutUs'})" >
+            <button 
+                v-if="ShowContent.contenido != 'home'"
+                class="item home" 
+                @click="callChangeContent({ contenido: 'home'})" 
+            >
+                {{ idioma.home }}
+            </button>
+
+            <div class="item-products">
+                <button class="">{{ idioma.products }}</button>
+                <div class="dropdown products">
+                    <button
+                        v-for="producto in productos"
+                        :key="producto.id"
+                        class="product"
+                        :value="producto.id"
+                        @click="
+                            callChangeContent({ contenido: 'producto', id: producto.id })
+                        "
+                    >
+                        {{ producto.nombre }}
+                    </button>
+                </div>
+            </div>
+            <button class="item" @click="callChangeContent({ contenido: 'aboutUs'})">
                 {{ idioma.aboutUs }}
             </button>
-            <button class="item boton-idioma" @click="callToggleLenguage" >
-                <img v-if="currentLenguage === 'es'" :src="idioma.language" alt="es">
-                <img v-else :src="idioma.language" alt="en">
+            <button class="item boton-idioma" @click="callToggleLenguage">
+                <img v-if="currentLanguage === 'es'" :src="idioma.language" alt="es" />
+                <img v-else :src="idioma.language" alt="en" />
+            </button>
+            <button class="item" @click="callChangeContent({ contenido: 'cart' })">
+                <img src="../assets/img/logos/carrito.png" alt="cart"> {{ idioma.cart }}
+            </button>
+            <button
+                v-if="islogged"
+                class="item"
+                @click="callChangeContent({ contenido: 'perfil' })"    
+            >
+                <img src="../assets/img/logos/cuenta.png" alt="perfil"> {{ usuario.nombre }}
+            </button>
+            <button
+                v-else
+                class="item"
+                @click="callChangeContent({ contenido: 'logIn' })"
+            >
+                <img src="../assets/img/logos/cuenta.png" alt="perfil"> {{ idioma.loggin }}
             </button>
         </div>
     </nav>
 </template>
 
 <script lang="js" setup>
-    import { ref, defineProps, defineEmits } from 'vue';
+    import { inject, ref } from 'vue';
+    import $ from 'jquery';
 
     const props = defineProps({
-        showContent: {
+        productos: {
+            type: Array,
+            required: true,
+        },
+        ShowContent: {
             type: Object,
         },
         currentLenguage:{
@@ -25,14 +71,30 @@
         },
     });
 
+    $(document).ready(function (){
+        $('.product').click(function (){
+            let product = $(this).val();
+        });
+    });
+
     const languageText = {
         english:{
+            home: 'Home',
+            products: 'Products',
             aboutUs: 'About Us',
+            cart: 'Cart',
             language: '/src/assets/en.png',
+            perfil: 'My profile',
+            loggin: 'Log in',
         },
         spanish:{
+            home: 'Inicio',
+            products: 'Productos',
             aboutUs: 'Sobre Nosotros',
+            cart: 'Carrito',
             language: '/src/assets/es.png',
+            perfil: 'Mi perfil',
+            loggin: 'Iniciar Sesión',
         },
     };
 
@@ -44,19 +106,21 @@
 
     const emit = defineEmits(['changeContent', 'toggleLenguage']);
     function callToggleLenguage(){
+        // Call the function passed from the parent component
         emit('toggleLenguage');
         if(props.currentLenguage === 'es'){
             idioma.value = languageText.english;
         } else {
             idioma.value = languageText.spanish;
         }
-
-        console.log("Se cambio ek idioma a: ", props.currentLenguage);
+            console.log("Se cambio ek idioma a: ", props.currentLenguage);
     }
     function callChangeContent(contenido){
         // Call the function passed from the parent component
         emit('changeContent', contenido);
     }
+    const islogged = inject('islogged');
+    const usuario = inject('usuario');
 </script>
 
 <style scoped>
---
+++ src/components/Producto.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="product-card">
+            <img class="image-product" :src="producto.imagen" alt="" />
+            <div class="product-info">
+                <div class="info">
+                    <h2>{{ producto.nombre }}</h2>
+                    <p v-if="currentLanguage == 'es'">{{ producto.descripcion_es }}</p>
+                    <p v-else>{{ producto.descipcion_en }}</p>
+                </div>
+                <div class="precio-ctn">
+                    <p class="precio">{{ producto.precio }}€</p>
+                    <button
+                        v-if="producto.desponibilidad"
+                        @click="agregarAlCarrito()"
+                        class="btn-add-cart"
+                    >
+                        {{ 
+                            currentLanguage === 'es' ? 'Agregar al carrito' : 'Add to Cart' 
+                        }}
+                    </button>
+                    <button
+                        v-if="!producto.disponibilidad"
+                        class="btn-add-cart btn-agotado"
+                    >
+                        {{ currentLanguage === 'es' ? 'Agotado' : 'Sold Out' }}
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps, inject, ref} from 'vue';
+    import $ from 'jquery';
+
+    const currentLanguafe = inject('currentLanguage');
+    const usuario_id = inject('usuario').value.id;
+</script>
